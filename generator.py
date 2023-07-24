466673832a7219ecdcb2fa46b52de720c052d529
luca.canali <lucacanali.dev@gmail.com>
2023-07-24 23:38:05 +0200

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,8 +1,9 @@
 import random
 import string
 import tkinter
+import threading
 
-def generate_password(length=12):
+def generate_password(length):
     # Definisci i caratteri che saranno utilizzati per generare la password
     characters = string.ascii_letters + string.digits + string.punctuation
 
@@ -10,12 +11,22 @@ def generate_password(length=12):
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-if __name__ == "__main__":
-    password_length = 12  # Puoi specificare la lunghezza desiderata della password
-    generated_password = generate_password(password_length)
-    print("Password generata:", generated_password)
+def start():
+    selected_number = int(input.get())
+    password = generate_password(selected_number)
+    output.delete(1.0, tkinter.END)  # Cancella il testo precedente nell'elemento di testo
+    output.insert(tkinter.END, password)  # Inserisci la password generata nell'elemento di testo
 
 root = tkinter.Tk()
 root.title("Password generator by Luca Canali")
+
+input = tkinter.Entry()
+input.grid(row='0', column='0')
+
+output = tkinter.Text()
+output.grid(row='1', column='0')
+
+button = tkinter.Button(text="Generate", command=start)
+button.grid(row='0', column='1')
 
 root.mainloop()
